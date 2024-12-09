586d29e474c1d059f9678ad6c8ecb8b61dfefbd2
Chris <chrisbreuer93@gmail.com>
2024-12-09 17:47:08 +0100

    chore: initial docs
    chore: add scripts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'ts-collect',
+  description: 'A simple, fast, and secure TLS certificate manager for your projects.',
+  cleanUrls: true,
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Docs', link: '/intro' },
+      { text: 'Changelog', link: 'https://github.com/stacksjs/tlsx/releases' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Get Started',
+        items: [
+          { text: 'Introduction', link: '/intro' },
+          { text: 'Install', link: '/install' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/stacksjs/ts-collect' },
+      { icon: 'bluesky', link: 'https://bsky.app/profile/chrisbreuer.dev' },
+      { icon: 'twitter', link: 'https://twitter.com/stacksjs' },
+    ],
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,29 @@
+---
+layout: home
+
+hero:
+  name: "ts-collect"
+  text: "A TypeScript Collection Library"
+  tagline: "Powerful, yet lightweight, Laravel-like Collections written for TypeScript"
+  actions:
+    - theme: brand
+      text: Documentation
+      link: /intro
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/stacksjs/ts-collect
+
+features:
+  - title: Familiar API
+    icon: 👪
+    details: "Laravel-like API for working with collections in TypeScript."
+  - title: Lightweight
+    icon: 🚀
+    details: "Small footprint and performance optimized."
+  - title: Powerful
+    icon: 💪
+    details: "Rich set of methods for working with collections."
+  - title: Fully Typed
+    icon: 🏗️
+    details: "Written in and for TypeScript, with full type support."
+---
